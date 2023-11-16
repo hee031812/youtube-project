8c68f39c3294f764ad213f1fdb213149ef6af601
KIMHEEJIN <hee031812@gmail.com>
2023-11-16 19:06:57 +0900

    2023.11.16

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { fetchFromAPI } from '../utils/api';
+import VideoSearch from '../components/video/VideoSearch';
+import { CiRead } from "react-icons/ci";
+import { VscThumbsup } from "react-icons/vsc";
+import { CiViewList } from "react-icons/ci";
 
-const channel = () => {
+
+
+
+
+const Channel = () => {
+    const { channelId } = useParams();
+    const [channelDetail, setChannelDetail] = useState();
+    const [channelVideo, setChannelVideo] = useState([]);
+    // const [loading, setLoading] = useState(true)
+    // const [nextPageToken, setNextPageToken] = useState(null);
+
+    useEffect(() => {
+        const fetchResults = async () => {
+            try {
+                const data = await fetchFromAPI(`channels?part=snippet&id=${channelId}`);
+                // console.log(data)
+                setChannelDetail(data.items[0]);
+                const videoData = await fetchFromAPI(`search?channelId=${channelId}&part=snippet&order=date`);
+                // console.log(videoData);
+                setChannelVideo(videoData.items);
+            } catch (error) {
+                console.log("Error -> ", error);
+            }
+        }
+        fetchResults();
+    }, [channelId])
     return (
-        <div>channel</div>
+        <section id='channelPage'>
+            {channelDetail && (
+
+                <div className="channel__inner">
+                    <div className="channel__header" style={{ backgroundImage: `url(${channelDetail.brandingSettings.image.bannerExternalUrl})` }}>
+                        <div className='circle'>
+                            <img src={channelDetail.snippet.thumbnails.high.url} alt={channelDetail.snippet.title} />
+                        </div>
+                    </div>
+                    <div className="channel__info">
+                        <h3 className='title'>{channelDetail.snippet.title}</h3>
+                        <p className='desc'>{channelDetail.snippet.description}</p>
+                        <div className='info'>
+                            <span className='subscriberCount'><VscThumbsup />{channelDetail.statistics.viewCount}</span>
+                            <span className='videoCount'><CiViewList />{channelDetail.statistics.videoCount}</span>
+                            <span className='viewCount'><CiRead />{channelDetail.statistics.subscriberCount}</span>
+                        </div>
+                    </div>
+
+
+                    <div className="channel__video video__inner search">
+                        <VideoSearch videos={channelVideo} layout="channel" />
+                    </div>
+                    <div className="channel__more">
+                    </div>
+                </div>
+            )
+            }
+
+        </section >
     )
 }
 
-export default channel
+export default Channel
---
+++ src/pages/Video.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/api';
+import ReactPlayer from 'react-player';
+import { GoHeart } from "react-icons/go";
+import { IoMdEye } from "react-icons/io";
+
+const Video = () => {
+
+    const { videoId } = useParams();
+    const [videoDetail, setVideoDetail] = useState(null);
+    // const [comments, setComments] = useState([]);
+
+
+    useEffect(() => {
+        fetchFromAPI(`videos?part=snippet,statistics&id=${videoId}`)
+            .then((data) => {
+                console.log(data)
+                setVideoDetail(data.items[0]);
+            })
+    }, [videoId])
+    return (
+        <section id='videoPage'>
+            <h2 className='blind'>비디오</h2>
+            {videoDetail && (
+                <div className='video__view'>
+                    <div className='video__play'>
+                        <ReactPlayer
+                            playing={true}
+                            url={`https://www.youtube.com/watch?v=${videoId}`}
+                            width='100%'
+                            height='100%'
+                            style={{ position: 'absolute', top: 0, left: 0 }}
+                        />
+                    </div>
+                    <div className='video__info'>
+                        <h2 className='video__title'>{videoDetail.snippet.title}</h2>
+                        <div className='video__channel'>
+                            <div className='id'>
+                                <Link to={`/channel/${videoDetail.snippet.channelId}`}>{videoDetail.snippet.channelTitle}</Link>
+                            </div>
+                            <div className="count">
+                                <span className='view'><IoMdEye />조회수:{videoDetail.statistics.viewCount}</span>
+                                <span className='like'><GoHeart />좋아요:{videoDetail.statistics.likeCount}</span>
+                                <span className='commnet'>{videoDetail.statistics.commnetCount}</span>
+                            </div>
+                        </div>
+                        <div className="video__desc">
+                            {videoDetail.snippet.description}
+                            {videoDetail.snippet.description}
+                        </div>
+                    </div>
+                </div>
+            )}
+
+        </section>
+    )
+}
+
+export default Video
